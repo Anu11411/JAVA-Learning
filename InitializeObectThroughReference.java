6b7a475231d5ade711b3832d0a5b92db452d010d
Anupriya Rani <anupriyarani2002@gmail.com>
2024-08-23 12:00:38 +0530

    Add files via upload

---
+++ InitializeObectThroughReference.java
@@ -0,0 +1,17 @@
+class Student{
+// declaring instance variable
+    int id;
+    String name;
+
+}
+class TestStudent{
+
+    public static void main(String[] args) {
+ //creating objects
+     Student s1 = new Student();
+//initializing objects
+     s1.id = 8049;
+     s1.name = "Anupriya Rani";
+     System.out.println(s1.id+ " "+ s1.name);   
+    }
+}
