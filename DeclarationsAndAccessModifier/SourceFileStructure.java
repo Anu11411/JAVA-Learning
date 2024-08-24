60b9c0bbedb296e85c3542c4afd7404bb0999762
Anupriya Rani <anupriyarani2002@gmail.com>
2024-08-25 01:28:46 +0530

    Add files via upload

---
+++ DeclarationsAndAccessModifier/SourceFileStructure.java
@@ -0,0 +1,24 @@
+package DeclarationsAndAccessModifier;
+
+/*
+ * a java program can contain any number of classes but atmost
+ * one class can be declared as public , if there is public class then name of the program and name of the public class must be matched otherwise we will get complie time error
+ */
+class A{
+
+}
+class B{
+
+}
+class C{
+
+}
+/*
+ * If there is no public class then we can use any name and there are no restriction
+ * like A.java
+ *      B.java
+ *      C.java
+ *      anu.java
+ *--- if B class is public then name of the program is B.java otherwise we will get compile time error 
+ ---if class B and C declared as public and the name of the program is suppose B.java then we will get compile time error 
+ */
---
+++ DeclarationsAndAccessModifier/SourceFileStructure2.java
@@ -0,0 +1,24 @@
+package DeclarationsAndAccessModifier;
+ class A{
+    public static void main(String[] args) {
+        System.out.println("A class main");
+    }
+ }
+ class B{
+    public static void main(String[] args) {
+        System.out.println("B class main");
+    }
+ }
+ class C{
+    public static void main(String[] args) {
+        System.out.println("C class main");
+    }
+ }
+ class D{
+
+ }
+ /*
+  * Whenever we are trying to compile a java program for every class present in the
+  program a separte .class file be geneated like A.class b.class C.class D.class
+  
+  */
